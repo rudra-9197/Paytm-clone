d08c8f61f729823fdb0b7895e41327c645c7ead0
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2022-09-15 01:40:44 +0530

    Update style.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Roboto': "'Roboto',serif",
+      },
+
+
+    },
   },
   plugins: [],
 }
